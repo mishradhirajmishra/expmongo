8dc7bdad25a9da9ff61aeec735b600a0f31dd588
mishradhirajmishra <dhiraj.siswa.mishra@gmail.com>
2019-10-30 19:17:32 +0530

    put change

--- routes/router.js
+++ routes/router.js
@@ -4,7 +4,7 @@ const cont = require('../controller/admin');
 /* GET home page. */
 router.get('/', cont.index );
 router.get('/add', cont.addGet );
-router.post('/', cont.addPost );
+router.post('/add', cont.addPost );
 router.get('/edit/:id', cont.editGet );
 router.put('/update/:id', cont.updatePost );
 router.delete('/:id',cont.delete,cont.index );
